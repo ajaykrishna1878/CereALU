f95ee26d3054da071045d9b7aef1921955b797ab
Ajay Krishna <81514476+ajaykrishna1878@users.noreply.github.com>
2022-06-08 18:50:47 +0530

    Update CereALU.h

--- CereALU.h
+++ CereALU.h
@@ -1,5 +1,8 @@
 #include <stdint.h>
 
+#ifndef __CEREALU_H__
+#define __CEREALU_H__
+
 uint32_t Add(uint32_t a, uint32_t b);
 uint32_t Subtract(uint32_t a, uint32_t b);
 uint32_t Multiply(uint32_t a, uint32_t b);
@@ -8,3 +11,5 @@ uint32_t AddFPGA();
 uint32_t SubtractFPGA();
 uint32_t MultiplyFPGA();
 uint32_t DivideFPGA();
+
+#endif
