6f591676240c6ab5c920e12cc26a410ec45767e5
Ajay Krishna <81514476+ajaykrishna1878@users.noreply.github.com>
2022-06-08 18:48:32 +0530

    Create Test.c

---
+++ Test.c
@@ -0,0 +1,10 @@
+#include "CereALU.h"
+#include <stdio>
+
+int main(){
+  printf("Add(4, 2) = %d\n", Add(4, 2));
+  printf("Subtract(4, 2) = %d\n", Subtract(4, 2));
+  printf("Multiply(4, 2) = %d\n", Multiply(4, 2));
+  printf("Divide(4, 2) = %d\n", Divide(4, 2));
+  return 0;
+}
